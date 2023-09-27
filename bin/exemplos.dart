c5584f57c84f2d3cd6bf6934b7221dfa3d462260
layon <lyonmfm@gmail.com>
2023-09-27 09:32:19 -0300

    teste

---
+++ bin/exemplos.dart
@@ -0,0 +1,5 @@
+import 'package:exemplos/exemplos.dart' as exemplos;
+
+void main(List<String> arguments) {
+  print('Hello world: ${exemplos.calculate()}!');
+}
---
+++ test/exemplos_test.dart
@@ -0,0 +1,8 @@
+import 'package:exemplos/exemplos.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
